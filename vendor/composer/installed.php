3a6d0fc246c177b66cafdd32aca72fd3da34a900
Ahmad Siasa <ahmadsiasa45@gmail.com>
2026-02-10 12:38:43 +0300

    third

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '90d880e834a12220be27cbad7276019f2fb8858b',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -446,9 +446,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '90d880e834a12220be27cbad7276019f2fb8858b',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
